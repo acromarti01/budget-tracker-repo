6752264b019c048a91485ea3b1fc3e535cd1fde2
Anthony Cromartie <acromarti@gmail.com>
2021-10-02 22:41:50 -0500

    created checkDatabase function in db.js

--- public/db.js
+++ public/db.js
@@ -1,6 +1,6 @@
 let db;
 
-const request = indexedDB.open("BudgetDB");
+const request = indexedDB.open("BudgetTrackerDB");
 
 request.onupgradeneeded = function (event) {
   db = event.target.result;
@@ -20,18 +20,18 @@ request.onerror = function (event) {
 };
 
 function saveRecord(record) {
-  // create a transaction on the pending db with readwrite access
   const transaction = db.transaction(['BudgetStore'], 'readwrite')
-  // access your pending object store
   const store = transaction.objectStore('BudgetStore');
-  // add record to your store with add method.
   store.add(record);
 }
 
 function checkDatabase() {
   // open a transaction on your pending db
+  const transaction = db.transaction(['BudgetStore'], 'readwrite')
   // access your pending object store
+  const store = transaction.objectStore('BudgetStore');
   // get all records from store and set to a variable
+  const getAll = store.getAll();
 
   getAll.onsuccess = function () {
     if (getAll.result.length > 0) {
@@ -46,8 +46,11 @@ function checkDatabase() {
         .then((response) => response.json())
         .then(() => {
           // if successful, open a transaction on your pending db
+          const transaction = db.transaction(['BudgetStore'], 'readwrite')
           // access your pending object store
+          const store = transaction.objectStore('BudgetStore');
           // clear all items in your store
+          store.clear();
         });
     }
   };
