f0b30657c73d2edab9dcc50870587add58422264
Anthony Cromartie <acromarti@gmail.com>
2021-10-03 14:16:26 -0500

    deploy to Heroku

--- server.js
+++ server.js
@@ -15,26 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-const serverStart = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    });
-
-    await app.listen(PORT, () => {
-      console.log(`App running on port ${PORT}!`);
-    });
-  }
-  catch (err) {
-    console.log(err);
-  }
-}
-
+mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
 
 // routes
 app.use(require("./routes/api.js"));
 
-serverStart();
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
